646b74b9b63420f1136e7ed22a5ce543be6822aa
ThatCreeper <creeper_host@yahoo.com>
2019-12-29 16:35:18 -0600

    Add a lot of features

--- index.js
+++ index.js
@@ -1,18 +1,52 @@
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, Menu, shell, dialog} = require('electron')
 const path = require('path');
+const fs = require('fs');
 let mainWindow
+global.data = {
+  "filename": null,
+  "options": [
+    {"type":"text","name":"name"},
+    {"type":"text","name":"where"}
+  ],
+  "paintings": [
+    {"name":"Test1", "where":"Over The Rainbow"}
+  ]
+}
 
 function createWindow () {
+  var menu = Menu.buildFromTemplate([
+    {
+      label: 'Menu',
+      submenu: [
+        {label:'New', click(){newFile()}},
+        {label:'Open', click(){openFile()}},
+        {label:'Save', click(){saveFile()}},
+        {label:'Save As', click(){saveFileAs()}},
+        {label:'Exit', click(){app.quit()}},
+        {label:'Reload', click(){mainWindow.reload()}}
+      ]
+    },
+    {
+      label: 'Help!',
+      submenu: [
+        {label:'Report an issue', click(){shell.openExternal("https://github.com/ThatCreeper/arter/issues/new")}},
+        {label:'View source code', click(){shell.openExternal("https://github.com/ThatCreeper/arter")}},
+        {label:'View issues', click(){shell.openExternal("https://github.com/ThatCreeper/arter/issues")}}
+      ]
+    }
+  ])
+  Menu.setApplicationMenu(menu); 
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    frame: false,
     //icon: path.join(__dirname, "icon.png"),
     webPreferences: {
         nodeIntegration: true
     }
   })
   mainWindow.loadFile('index.html')
-  mainWindow.webContents.openDevTools();
+  //mainWindow.webContents.openDevTools();
   mainWindow.on('closed', function () {
     mainWindow = null
   })
@@ -27,3 +61,47 @@ app.on('window-all-closed', function () {
 app.on('activate', function () {
   if (mainWindow === null) createWindow()
 })
+
+function openFile(){
+  var openPath = dialog.showOpenDialogSync(
+    {
+      filters: [
+        { name: 'Arter Files', extensions: ['arter'] }
+      ],
+      properties: ['openFile']
+    }
+  )
+  //alert(openPath)
+  mainWindow.webContents.openDevTools()
+  console.log(openPath)
+  if (openPath == undefined) return;
+  var data = fs.readFileSync(openPath[0],{encoding:"utf8"});
+  console.log(data)
+  global.data = JSON.parse(data)
+  global.data.filename = openPath[0]
+}
+
+function saveFileAs(){
+  var savePath = dialog.showSaveDialogSync(
+  {
+    filters: [
+      {name:'Arter Files', extensions: ['arter']}
+    ]
+  })
+  if (savePath == undefined) return;
+  if (savePath.substr(savePath.length-6).toLowerCase()==".arter"){
+    global.data.filename = savePath
+  } else {
+    global.data.filename = savePath+'.arter';
+  }
+  console.log(savePath)
+  fs.writeFileSync(global.data.filename,JSON.stringify(global.data))
+}
+
+function saveFile() {
+  if (global.data.filename == null) {
+    saveFileAs()
+    return;
+  }
+  fs.writeFileSync(global.data.filename,JSON.stringify(global.data))
+}
--- main.js
+++ main.js
@@ -1,5 +1,39 @@
-var { Titlebar, Color } = require('custom-electron-titlebar')
+let remote
+var _ = require('underscore')
+window.onload = ()=>{
+	const customTitlebar = require('custom-electron-titlebar');
+	remote = require('electron').remote
+	//console.log(remote.getGlobal('data'))
+	new customTitlebar.Titlebar({
+		backgroundColor: customTitlebar.Color.fromHex('#1598bf')
+	});
+	document.getElementById("addnew").onclick = ()=>showForm()
+	document.getElementById("overlay").onclick = ()=>hideForm()
+}
 
-new Titlebar({
-	backgroundColor: Color.fromHex('#ECECEC')
-});
+function showForm() {
+	var newInnerHtml = "";
+	remote.getGlobal("data").options.forEach((data)=> {
+		newInnerHtml+="<input type=\""+data.type+"\" name=\""+data.name+"\" placeholder=\""+data.name+"\"/><br/>"
+	})
+	newInnerHtml+="<button>Add</button>"
+	document.getElementById("myform").innerHTML=newInnerHtml;
+	document.getElementById("overlay").style.display = "block"
+	document.getElementById("newmenu").style.display = "block"
+}
+
+function hideForm() {
+	document.getElementById("overlay").style.display = "none"
+	document.getElementById("newmenu").style.display = "none"
+}
+
+function submitForm() {
+	var object = {};
+	new FormData(document.getElementById('myform')).forEach((value, key) => {object[key] = value.toString});
+	var remote_data = remote.getGlobal('data').paintings
+	remote_data.push(object)
+	remote.getGlobal('data').paintings = remote_data
+	hideForm()
+	console.log(remote.getGlobal('data'))
+	return false;
+}
